cf7bb6aa58c286158fcb4351341cc4e609369ca5
LOVEPREET <lovepreet.singh@adp.com>
2020-08-07 12:49:03 -0700

    implemented the stateFulWidget

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
+class App extends StatefulWidget{
+  @override
+  State<App> createState() {
+    return Appstate();
+  }
 
-class App extends StatelessWidget{
+}
 
+class Appstate extends State<App> {
+
+int counter = 0;
    @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Welcome to Pics App."),)
-        ,floatingActionButton:
-         FloatingActionButton(onPressed: () => print("Hello from Flutter")
-         ,child: Icon(Icons.photo_library)
+        ,body: Text("$counter"),
+        floatingActionButton:
+         FloatingActionButton(
+           child: Icon(Icons.photo_library),
+           // On press of button we are calling 
+           //Set state method from Parent Class State
+           // to maintain the value of counter from 
+           //previous click of the button
+           onPressed:  () => setState(() => counter +=1 )
          ,)
-         ,),
-          );
+         ,)
+         ,);
   }
 }
